483469cbb5348786607e930668a3f5dcf0c37845
piechotadawid2000@gmail.com <piechotadawid2000@gmail.com>
2023-07-11 16:45:27 +0200

    2nd nav bar

--- aircon/src/components/navigation.tsx
+++ aircon/src/components/navigation.tsx
@@ -2,6 +2,33 @@ import { styled } from "styled-components";
 import { useNavigate } from "react-router-dom";
 import Logo from "../assets/pulse-logo.png";
 
+const ContactContainer = styled.div`
+  height: 100px;
+  width: 100%;
+  background-color: black;
+`;
+
+const ContactInner = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  gap: 20px;
+  margin-left: 20%;
+  color: white;
+`;
+
+const ContactInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  gap: 20px;
+  margin-right: 20%;
+  color: white;
+`;
+
 const NavContainer = styled.div`
   height: 50px;
   width: 100%;
@@ -9,11 +36,12 @@ const NavContainer = styled.div`
 `;
 
 const NavLinksContainer = styled.div`
+  height: 100%;
   display: flex;
   flex-direction: row;
   align-items: center;
+  justify-content: center;
   gap: 20px;
-  margin-left: 20%;
 `;
 
 const NavLinks = styled.p`
@@ -26,16 +54,27 @@ export const Navigation = () => {
   const navigate = useNavigate();
   return (
     <>
+      <ContactContainer>
+        <ContactInner>
+          <div>
+            <img
+              onClick={() => navigate("/home")}
+              style={{ height: "50px" }}
+              src={Logo}
+            ></img>
+          </div>
+          <ContactInfo>
+            <p>dawidpiechota@gmail.com</p>
+            <p>000-000-000</p>
+            <p>Mon-Sun: 10am - 3pm</p>
+          </ContactInfo>
+        </ContactInner>
+      </ContactContainer>
       <NavContainer className="nav-container">
         <NavLinksContainer className="nav-links-container">
-          <img
-            onClick={() => navigate("/home")}
-            style={{ height: "50px" }}
-            src={Logo}
-          ></img>
-          {/* <NavLinks onClick={() => navigate("/home")}>Home</NavLinks> */}
-          <NavLinks onClick={() => navigate("/aboutus")}>About Us</NavLinks>
-          <NavLinks onClick={() => navigate("/showcase")}>Our Work</NavLinks>
+          <NavLinks onClick={() => navigate("/home")}>Home</NavLinks>|
+          <NavLinks onClick={() => navigate("/aboutus")}>About Us</NavLinks>|
+          <NavLinks onClick={() => navigate("/showcase")}>Our Work</NavLinks>|
           <NavLinks onClick={() => navigate("/stock")}>Stock</NavLinks>
         </NavLinksContainer>
       </NavContainer>
